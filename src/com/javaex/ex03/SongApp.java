da6518b06237726c95ec53a30e01b87f8ae091ff
whjkh <whjkh@DESKTOP-O9T8HTK>
2020-11-24 16:06:47 +0900

    2020. 11. 24 homework

--- src/com/javaex/ex03/SongApp.java
+++ src/com/javaex/ex03/SongApp.java
@@ -8,46 +8,31 @@ public class SongApp {
 		Song Gd = new Song();
 		Song Bk = new Song();
 		
-		Iu.setTitle("좋은날");
-		String title01 = Iu.getTitle();
-		Iu.setArtist("아이유");
-		String artist01 = Iu.getArtist();
-		Iu.setAlbum("Real");
-		String album01 = Iu.getAlbum();
-		Iu.setComposer("이민수");
-		String composer01 = Iu.getComposer();
-		Iu.setYear(2010);
-		int year01 = Iu.getYear();
+		Iu.setTitle("좋은날");		
+		Iu.setArtist("아이유");		
+		Iu.setAlbum("Real");		
+		Iu.setComposer("이민수");		
+		Iu.setYear(2010);		
 		Iu.setTrack(3);
-		int track01 = Iu.getTrack();
+	
 		Iu.showInfo();
 		
-		Gd.setTitle("거짓말");
-		String title02 = Gd.getTitle();
-		Gd.setArtist("빅뱅");
-		String artist02 = Gd.getArtist();
-		Gd.setAlbum("Always");
-		String album02 = Gd.getAlbum();
-		Gd.setComposer("G-Dragon");
-		String composer02 = Gd.getComposer();
-		Gd.setYear(2007);
-		int year02 = Gd.getYear();
+		Gd.setTitle("거짓말");		
+		Gd.setArtist("빅뱅");		
+		Gd.setAlbum("Always");		
+		Gd.setComposer("G-Dragon");		
+		Gd.setYear(2007);		
 		Gd.setTrack(2);
-		int track02 = Gd.getTrack();
+		
 		Gd.showInfo();
 		
-		Bk.setTitle("벚꽃엔딩");
-		String title03 = Bk.getTitle();
-		Bk.setArtist("버스커버스커");
-		String artist03 = Bk.getArtist();
-		Bk.setAlbum("버스커버스커 1집");
-		String album03 = Bk.getAlbum();
-		Bk.setComposer("장범준");
-		String composer03 = Bk.getComposer();
-		Bk.setYear(2012);
-		int year03 = Bk.getYear();
+		Bk.setTitle("벚꽃엔딩");		
+		Bk.setArtist("버스커버스커");		
+		Bk.setAlbum("버스커버스커 1집");		
+		Bk.setComposer("장범준");		
+		Bk.setYear(2012);		
 		Bk.setTrack(4);
-		int track03 = Bk.getTrack();
+		
 		Bk.showInfo();
 
 	}
