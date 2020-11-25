96f946df3fdd312fb8aaf2a66bbae75642269ef7
whjkh <whjkh@DESKTOP-O9T8HTK>
2020-11-25 18:12:43 +0900

    2020 11. 25 homework

---
+++ src/com/javaex/ex08/TV.java
@@ -0,0 +1,76 @@
+package com.javaex.ex08;
+
+public class TV {
+
+	private int channel;
+	private int volume;
+	private boolean power;
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public boolean getPower() {
+		return power;
+	}
+
+	public TV(int channel, int volume, boolean power) {
+		this.channel = 7;
+		this.volume = 20;
+		this.power = false;
+	}
+
+	public TV() {
+
+	}
+
+	public void power(boolean on) {
+		this.power = on;
+	}
+
+	public void channel(int channel) {
+		this.channel = channel;
+		if (channel > 255) {
+			this.channel = 255;
+		} else if (channel < 1) {
+			this.channel = 1;
+		}
+	}
+
+	public void channel(boolean channel) {
+		if (channel) {
+			this.channel++;
+		} else {
+			this.channel--;
+		}
+	}
+
+	public void volume(int volume) {
+		this.volume = volume;
+		if (volume > 100) {
+			this.volume = 100;
+		} else if (volume < 0) {
+			this.volume = 0;
+		}
+	}
+
+	public void volume(boolean volume) {
+		if (volume) {
+			this.volume++;
+		} else {
+			this.volume--;
+		}
+	}
+
+	void status() {
+		if (power) {
+			System.out.println("[전원: on, 채널: " + channel + ", 볼륨: " + volume + "]");
+		} else {
+			System.out.println("[전원: off, 채널: " + channel + ", 볼륨: " + volume + "]");
+		}
+	}
+}
---
+++ src/com/javaex/ex08/TVApp.java
@@ -0,0 +1,35 @@
+package com.javaex.ex08;
+
+public class TVApp {
+
+	public static void main(String[] args) {
+		
+		TV tv = new TV(7, 20, false);		
+		tv.status();
+		System.out.println("==========================");
+		
+		tv.power(true);
+		tv.volume(100);
+		tv.status();
+		System.out.println("==========================");
+		
+		tv.volume(true);
+		tv.status();
+		System.out.println("==========================");
+		
+		tv.channel(255);
+		tv.status();
+		System.out.println("==========================");
+		
+		tv.channel(true);
+		tv.channel(true);
+		tv.channel(true);
+		tv.status();
+		System.out.println("==========================");
+		
+		tv.power(false);
+		tv.status();
+
+	}
+
+}
